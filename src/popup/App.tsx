51b261870ed365ce441b63a967969826486f8079
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2023-05-01 11:36:29 +0800

    feat: 🎸 adjust manifest

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,5 +1,21 @@
+import { css } from "@emotion/react";
+
 function App() {
-  return <div>youtube ads skipper</div>;
+  return (
+    <div
+      css={css`
+        height: 200px;
+        width: 200px;
+        display: grid;
+        justify-content: center;
+        align-items: center;
+        font-size: 20px;
+        font-weight: bold;
+      `}
+    >
+      <div>youtube ads skipper</div>
+    </div>
+  );
 }
 
 export default App;
